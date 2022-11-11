4c15b3a009d4baaf823db078ae462bb3e2751221
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-11-11 18:31:41 +0100

    :tada: initial commit

---
+++ src/AppRouter.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// Singleton (create only one single router in our app)
+export class AppRouter {
+  private static instance: express.Router;
+
+  static getInstance(): express.Router {
+    if (!AppRouter.instance) {
+      AppRouter.instance = express.Router();
+    }
+
+    return AppRouter.instance;
+  }
+}
